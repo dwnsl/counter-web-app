567028b7737df80e1b2f1a34b98be28b7bde1211
Daniel Wensel <danieljwensel@gmail.com>
2021-10-28 11:31:44 -0600

    changed navbar title

---
+++ src/components/nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+class NavBar extends React.Component {
+    render() { 
+        return (<nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+
+           Counter App <span className="badge badge-pill badge-secondary">
+                {this.props.totalCounters}
+                </span>
+            
+            </a>
+      </nav>);
+    }
+}
+ 
+export default NavBar;
